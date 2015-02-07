d442e3b59661dc75ce8e7cda52285871eb161275
nbkm8y5 <moreno.ronline@gmail.com>
2015-02-07 15:42:12 -0500

    Developer Cutting Board
    Base index file with Bootstrap, Jquery, and PHP to work on widgets.

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+?>
+<!DOCTYPE html>
+<!--[if IE 8]> <html class="no-js lt-ie9 ie8" lang="en"> <![endif]-->
+<!--[if IE 9]> <html class="ie9" lang="en"> <![endif]-->
+<!--[if IE 10]> <html class="ie10" lang="en"> <![endif]-->
+<!--[if (gt IE 10)|!(IE)]> <html lang="en"> <![endif]-->
+<html lang="en">
+
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- This is the metatag to ensure mobile platform functionality -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Rolando Moreno">
+        <meta title= "Developer Cutting Board">
+        <!-- Latest compiled and minified CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+        <link rel="icon" type="image/png" href="http://rolandomoreno.com/images/bhLogoMini.jpg">
+        
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+              <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+              <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    
+    <body>
+        
+      <section class="text-center">
+        <div class="container-fluid">
+          <h1 class="text-center">DEVELOPER CUTTING BOARD</h1>
+        </div>
+        <div class="container-fluid">
+        <p >Test to see how and individual image and paragraph can be edited.</p>
+        <img class="img-responsive" style="margin-left:auto;margin-right:auto" src="http://img1.etsystatic.com/002/0/5285890/il_570xN.357520463_k9ge.jpg">
+        </div>
+        <div class="container-fluid">
+            <h1>Links to current modifications below.</h1>
+        </div>
+      </section>
+
+      
+      <footer id="footer" class="container-fluid text-center">
+            <p>&copy <?php echo date("Y")?> Rolando Moreno</p>
+        </footer>
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    </body>
+    
+</html>    
